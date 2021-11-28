2ef2336b31eaf640159c7145b3c57fb1defa41a8
nilsv01 <nils.vogelpohl1@gmail.com>
2021-11-28 12:29:24 +0100

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useState } from 'react';
-import { Box, Button, FormControl, TextField} from '@material-ui/core';
+import { Box, Button, TextField, MuiThemeProvider, createTheme } from '@material-ui/core';
 import Axios from  'axios';
 
 function App() { 
@@ -9,176 +9,65 @@ function App() {
   const [visitorDaily, setVisitorDaily] = useState(0);
   const [visitorTotal, setVisitorTotal] = useState(0);
 
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: '#E33E7F'
+      }
+    }
+  });
 
   const addVisitor = () => {
-    Axios.post('http://localhost:3001/add').then(() =>
+    Axios.post('http://192.168.2.174:3001/add').then(() =>
     {console.log("success add");});
-    setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorMoment').then((response) => {setVisitorMoment(response.data);});
-    setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorDaily').then((response) => {setVisitorDaily(response.data);});
-    setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorTotal').then((response) => {setVisitorTotal(response.data);});
-    setTimeout(2000);
+    setTimeout(3000);
+    Axios.get('http://192.168.2.174:3001/getVisitorMoment').then((response) => {setVisitorMoment(JSON.parse(JSON.stringify(response.data))[0].visitorMoment);});
+    setTimeout(3000);
+    Axios.get('http://192.168.2.174:3001/getVisitorDaily').then((response) => {setVisitorDaily(JSON.parse(JSON.stringify(response.data))[0].visitorDaily);});
+    setTimeout(3000);
+    Axios.get('http://192.168.2.174:3001/getVisitorTotal').then((response) => {setVisitorTotal(JSON.parse(JSON.stringify(response.data))[0].visitorTotal);});
+    setTimeout(12000);
   }
-
+  
   const subVisitor = () => {
-    Axios.post('http://localhost:3001/sub').then(() =>
+    Axios.post('http://192.168.2.174:3001/sub').then(() =>
     {console.log("success sub");});
     setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorMoment').then((response) => {setVisitorMoment(response.data);});
+    Axios.get('http://192.168.2.174:3001/getVisitorMoment').then((response) => {setVisitorMoment(JSON.parse(JSON.stringify(response.data))[0].visitorMoment);});
     setTimeout(2000);
   }
 
   const resMoment = () => {
-    Axios.post('http://localhost:3001/resMoment').then(() =>
+    Axios.post('http://192.168.2.174:3001/resMoment').then(() =>
     {console.log("success add");});
     setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorMoment').then((response) => {setVisitorMoment(response.data);});
+    Axios.get('http://192.168.2.174:3001/getVisitorMoment').then((response) => {setVisitorMoment(JSON.parse(JSON.stringify(response.data))[0].visitorMoment);});
     setTimeout(2000);
   }
 
   const resDaily = () => {
-    Axios.post('http://localhost:3001/resDaily').then(() =>
+    Axios.post('http://192.168.2.174:3001/resDaily').then(() =>
     {console.log("success add");});
     setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorDaily').then((response) => {setVisitorDaily(response.data);});
+    Axios.get('http://192.168.2.174:3001/getVisitorDaily').then((response) => {setVisitorDaily(JSON.parse(JSON.stringify(response.data))[0].visitorDaily);});
     setTimeout(2000);
   }
 
   const resTotal = () => {
-    Axios.post('http://localhost:3001/resTotal').then(() =>
+    Axios.post('http://192.168.2.174:3001/resTotal').then(() =>
     {console.log("success add");});
     setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorTotal').then((response) => {setVisitorTotal(response.data);});    
+    Axios.get('http://192.168.2.174:3001/getVisitorTotal').then((response) => {setVisitorTotal(JSON.parse(JSON.stringify(response.data))[0].visitorTotal);});    
     setTimeout(2000);
   }
-
-  const getVisitor = () => {
-    
-    Axios.get('http://localhost:3001/getVisitorMoment').then((response) => {setVisitorMoment(response.data);});
-    setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorDaily').then((response) => {setVisitorDaily(response.data);});
-    setTimeout(2000);
-    Axios.get('http://localhost:3001/getVisitorTotal').then((response) => {setVisitorTotal(response.data);});
-  }
-
-  const getVisitorDaily = () => {
-    Axios.get('http://localhost:3001/getVisitorDaily').then((response) =>
-    { setVisitorDaily(response.data);});
-  }
-
-  const getVisitorTotal = () => {
-    Axios.get('http://localhost:3001/getVisitorTotal').then((response) =>
-    { setVisitorTotal(response.data);});
-  }
-
-  /*
-  constructor(props){
-    super(props);
-    this.state = { errorOpen: false, isLoading: false, customer: {}}
-    
-    this.numberMoment = 0;
-    this.numberDaily = 0;
-    this.numberTotal = 0;
-  }
-  
-  
-  state = {
-    momentVisitor: 0,
-    dailyVisitor: 0,
-    totalVisitor: 0
-  }
-   
-
-  addVisitor = async () => {
-    this.props.addMoment();
-    this.props.addDaily();
-    this.props.addTotal();
-  }
-
-  subVisitor = async () => {
-    this.props.subMoment();
-  }
-
-  resMoment = async () => {
-    this.props.resetMoment();
-  }
-
-  resDaily = async () => {
-    this.props.resetDaily();
-  }
-
-  resTotal = async () => {
-    this.props.resetTotal();
-  }
-
-  addVisitor = async () => {
-    try {
-      await AsyncStorage.setItem('momentVisitor', this.state.momentVisitor + 1);
-      await AsyncStorage.setItem('dailyVisitor', this.state.dailyVisitor + 1);
-      await AsyncStorage.setItem('totalVisitor', this.state.totalVisitor + 1);
-    } catch (error) {
-      // Error saving data
-    }
-  };
-  
-  subVisitor = async () => {
-    try {
-      await AsyncStorage.setItem('momentVisitor', this.state.momentVisitor - 1);
-    } catch (error) {
-      // Error saving data
-    }
-  };
-
-  resMoment = async () => {
-    try {
-      await AsyncStorage.setItem('momentVisitor', 0);
-    } catch (error) {
-      // Error saving data
-    }
-  };
-
-  resDaily = async () => {
-    try {
-      await AsyncStorage.setItem('dailyVisitor', 0);
-    } catch (error) {
-      // Error saving data
-    }
-  };
-
-  resTotal = async () => {
-    try {
-      await AsyncStorage.setItem('dailyTotal', 0);
-    } catch (error) {
-      // Error saving data
-    }
-  };
-
-  getVisitor = async () => {
-    try {
-      const constMoment = await AsyncStorage.getItem('momentVisitor');
-      const constDaily = await AsyncStorage.getItem('dailyVisitor');
-      const constTotal = await AsyncStorage.getItem('totalVisitor');
-
-      if (constTotal!==null){
-          this.setState({momentVisitor: constMoment});
-          this.setState({dailyVisitor: constDaily});
-          this.setState({totalVisitor: constTotal});
-      }
-
-    } catch (error) {
-      // Error retrieving data
-    }
-  };
-  render(){*/
 
   return (
+    <MuiThemeProvider theme={theme}>
     <div className="App">
       <header className="App-header">
         <Box className="App-Header">
           <h2 className="App-Dialog-Moment">Aktuelle Besucher</h2>
-          <div style={{ height: 300, width: '90%' }}>
+          <div style={{ height: 100, width: '90%' }}>
             <Button variant="outlined" color="primary" className ="App-Moment-Add-Button" onClick={addVisitor} >
                     +  </Button>
                     <TextField
@@ -186,7 +75,7 @@ function App() {
                             disabled={false}
                             label=""
                             type={Number}
-                            value={visitorDaily}
+                            value={visitorMoment}
                         />
             <Button variant="outlined" color="primary" className ="App-Moment-Sub-Button" onClick={subVisitor}>
                     -
@@ -196,26 +85,28 @@ function App() {
             </Button>
           </div>
           <h2 className="App-Dialog-Daily">Heutige Besucher</h2>
-          <div style={{ height: 300, width: '90%' }}>
+          <div style={{ height: 100, width: '90%' }}>
             <TextField
                             className ="App-Output-Daily" 
                             disabled={false}
                             label=""
                             type={Number}
-                            value={JSON.stringify(visitorDaily)}
+                            color="primary"
+                            value={visitorDaily}
                         />
             <Button variant="outlined" color="primary" className ="App-Daily-Reset-Button" onClick={resDaily}>
                     Reset
             </Button>
           </div>
           <h2 className="App-Dialog-Total">Gesamte Besucher</h2>
-          <div style={{ height: 300, width: '90%' }}>
+          <div style={{ height: 100, width: '90%' }}>
             <TextField
                             className ="App-Output-Total" 
                             disabled={false}
                             label=""
                             type={Number}
-                            value={JSON.stringify(visitorTotal)}
+                            color="secondary"
+                            value={visitorTotal}
                         />
             <Button variant="outlined" color="primary" className ="App-Daily-Total-Button" onClick={resTotal}>
                     Reset
@@ -224,30 +115,8 @@ function App() {
         </Box>
       </header>
     </div>
+    </MuiThemeProvider>
   );
  }
 
-
-/*
-const mapStateToProps = (state) => {
-  return{
-    moment: state.moment,
-    daily: state.daily,
-    total: state.total
-  }
-
-}
-const mapDisptachToProps = ()=> {
-  return{
-    addMoment,
-    addDaily,
-    addTotal,
-    resetMoment,
-    resetDaily,
-    resetTotal,
-    subMoment
-  }
-}
-*/
-//*export default connect(mapStateToProps, mapDisptachToProps())(App);
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,32 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
-import allReducer from './reducer';
 import reportWebVitals from './reportWebVitals';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { persistStore, persistReducer} from 'redux-persist';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
-import Storage  from 'redux-persist/lib/storage';
-import localStorage from 'redux-persist/es/storage';
 
-const persistConfig = {
-  key: 'root',
-  storage: localStorage,
-  stateReconciler: autoMergeLevel2 
- };
- const pReducer = persistReducer(persistConfig, allReducer);
- const store = createStore(pReducer);
- const persistor = persistStore(store);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store = {store}>
-      <PersistGate loading={<App/>} persistor={persistor}>
         <App />
-      </PersistGate>
-    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
